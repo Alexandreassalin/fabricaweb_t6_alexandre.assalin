e69681e1a55cfe2c7c907e2bf43afbbfc38b5ab7
Alexandre <limaassalin@hotmail.com>
2015-06-27 17:14:16 -0400

    Metodos de Cadastro, alteração, localizar, exclusao e autenticação no banco.

--- src/main/java/com/fabricadeprogramador/persistencia/dao/UsuarioDAO.java
+++ src/main/java/com/fabricadeprogramador/persistencia/dao/UsuarioDAO.java
@@ -2,32 +2,189 @@ package com.fabricadeprogramador.persistencia.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.naming.spi.DirStateFactory.Result;
 
 import com.fabricadeprogramador.persistencia.entidade.Usuario;
 import com.fabricadeprogramador.persistencia.jdbc.ConexaoFactory;
 
 public class UsuarioDAO {
 
-		private Connection con = ConexaoFactory.getConnection();
-		
-		public void cadastrar(Usuario usu){
-			
-			String sql = "INSERT INTO usuario (nome,login,senha) VALUES (?,?,md5(?))";
-			try (PreparedStatement preparador = con.prepareStatement(sql)){
-				
-				preparador.setString(1, usu.getNome());
-				preparador.setString(2, usu.getLogin());
-				preparador.setString(3, usu.getSenha());
-				
-				preparador.execute();
-				
-			} catch (SQLException e) {
-				
-				e.printStackTrace();
-			
-			}
+	Connection con = ConexaoFactory.getConnection();
+
+	public void cadastrar(Usuario usu) {
+
+		String sql = "INSERT INTO usuario (nome,login,senha) VALUES (?,?,md5(?))";
+
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+
+			preparador.setString(1, usu.getNome());
+			preparador.setString(2, usu.getLogin());
+			preparador.setString(3, usu.getSenha());
+
+			preparador.execute();
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+
 		}
+
+	}
+
+	public void alterar(Usuario usu) {
+
+		String sql = "UPDATE usuario SET nome = ?, login = ?, senha = md5(?) WHERE id = ?";
+		// Pegar a Conexão
+
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+
+			preparador.setString(1, usu.getNome());
+			preparador.setString(2, usu.getLogin());
+			preparador.setString(3, usu.getSenha());
+			preparador.setInt(4, usu.getId());
+
+			preparador.execute();
+
+		} catch (SQLException ex) {
+			throw new RuntimeException("Erro ao alterar o usuario", ex);
+		}
+
+	}
+
+	public void excluir(Integer id) {
+
+		// Montar o sql
+		String sql = "DELETE FROM usuario  WHERE id = ?";
+		// Pegando o statement
+
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+			preparador.setInt(1, id);
+
+			preparador.execute();
+
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao excluir", e);
+		}
+
+	}
+
+	public void salvar(Usuario usu) {
+
+		if (usu.getId() != null && usu.getId() > 0) {
+
+			alterar(usu);
+
+		} else {
+			cadastrar(usu);
+		}
+
+	}
+
+	public Usuario buscarPorId(Integer id) {
+
+		// Montar o sql
+		String sql = "SELECT * FROM  usuario  WHERE id = ?";
+		// Pegando o statement
+
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+
+			preparador.setInt(1, id);
+
+			ResultSet result = preparador.executeQuery();
+
+			if (result.next()) {
+
+				Usuario usuRetorno = new Usuario();
+				usuRetorno.setNome(result.getString("nome"));
+				usuRetorno.setLogin(result.getString("login"));
+				usuRetorno.setSenha(result.getString("senha"));
+				usuRetorno.setId(result.getInt("id"));
+
+				return usuRetorno;
+
+			} else {
+
+				System.out.println("Não Existe usuario com essa id!!!");
+				return null;
+			}
+
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao Buscar Id", e);
+		}
+
+	}
+
+	public List<Usuario> buscarTodos() {
+
+		// Montar o sql
+		String sql = "SELECT * FROM  usuario ORDER BY id ASC";//Order By metodo de ordenação Banco de dados
+		
+		// Pegando o statement
+
+		try (PreparedStatement preparador = con.prepareStatement(sql)) {
+
+			List<Usuario> listaRetorno = new ArrayList<Usuario>();
+			ResultSet result = preparador.executeQuery();
+
+			while (result.next()) {
+
+				Usuario usuRetorno = new Usuario();
+				usuRetorno.setNome(result.getString("nome"));
+				usuRetorno.setLogin(result.getString("login"));
+				usuRetorno.setSenha(result.getString("senha"));
+				usuRetorno.setId(result.getInt("id"));
+
+				listaRetorno.add(usuRetorno);
+			}
+			return listaRetorno;
+
+		} catch (SQLException e) {
+			throw new RuntimeException("Erro ao Buscar Todos", e);
+		}
+
+	}
 	
+	public Usuario autenticar(Usuario usu){
+		// Montar o sql
+		String sql = "SELECT * FROM  usuario WHERE login = ? AND senha = md5(?)";
+		// Pegando o statement
+
+				try (PreparedStatement preparador = con.prepareStatement(sql)) {
+
+				
+					preparador.setString(1, usu.getLogin());
+					preparador.setString(2, usu.getSenha());
+				
+					ResultSet result = preparador.executeQuery();
+
+						
+					if (result.next()) {
+
+						Usuario usuRetorno = new Usuario();
+						usuRetorno.setNome(result.getString("nome"));
+						usuRetorno.setLogin(result.getString("login"));
+						usuRetorno.setSenha(result.getString("senha"));
+						usuRetorno.setId(result.getInt("id"));
+
+						return usuRetorno;
+
+					} else {
+
+						System.out.println("Login incorreto ou senha incorretos!!!");
+						return null;
+					}
+
+
+				} catch (SQLException e) {
+					throw new RuntimeException("Erro ao Autenticar", e);
+				}
+		
+		
+	}
 	
 }
--- src/teste/java/cjweb/TestUsuarioDAO.java
+++ src/teste/java/cjweb/TestUsuarioDAO.java
@@ -1,26 +1,124 @@
 package cjweb;
 
+import java.util.List;
+import java.util.Scanner;
+
 import com.fabricadeprogramador.persistencia.dao.UsuarioDAO;
 import com.fabricadeprogramador.persistencia.entidade.Usuario;
 
 public class TestUsuarioDAO {
 
 	public static void main(String[] args) {
-		
-		testaCadastrar();
-		System.out.println("Cadastrado com sucesso");
 
-	}
-
-	public static void testaCadastrar(){
-		
-		Usuario usuCad = new Usuario();
-		usuCad.setNome("Carlos");
-		usuCad.setLogin("CAR");
-		usuCad.setSenha("123");
-		
+		Scanner teclado = new Scanner(System.in);
 		UsuarioDAO usuDao = new UsuarioDAO();
-		usuDao.cadastrar(usuCad);
+		Usuario usu = new Usuario();
+		// testAlterar(teclado, usuDao, usu);
+		// testExcluir(teclado, usu, usuDao);
+		// testSalvar(teclado, usuDao, usu);
+		// testBuscarPorId(teclado, usuDao);
+		//testBuscarTodos(usuDao);
+		testAutenticar(teclado, usuDao, usu);
 		
+		// no final fechar leitor
+		teclado.close();
 	}
+
+	public static void testSalvar(Scanner teclado, UsuarioDAO usuDao,
+			Usuario usu) {
+
+		System.out.println("Id do Usuario");
+		usu.setId(teclado.nextInt());
+
+		System.out.println("Nome do Usuario");
+		usu.setNome(teclado.next());
+
+		System.out.println("login Usuario");
+		usu.setLogin(teclado.next());
+
+		System.out.println("senha do Usuario");
+		usu.setSenha(teclado.next());
+
+		usuDao.salvar(usu);
+		System.out.println("Salvo com Sucesso");
+
+	}
+
+	public static void testAlterar(Scanner teclado, UsuarioDAO usuDao,
+			Usuario usu) {
+
+		System.out.println("Id do Usuario");
+		usu.setId(teclado.nextInt());
+
+		System.out.println("Nome do Usuario");
+		usu.setNome(teclado.next());
+
+		System.out.println("login Usuario");
+		usu.setLogin(teclado.next());
+
+		System.out.println("senha do Usuario");
+		usu.setSenha(teclado.next());
+
+		usuDao.alterar(usu);
+		System.out.println("Alterado com Sucesso");
+
+	}
+
+	public static void testExcluir(Scanner teclado, Usuario usu,
+			UsuarioDAO usuDao) {
+
+		System.out.println("Id do Usuario");
+		Integer id = teclado.nextInt();
+		usuDao.excluir(id);
+		System.out.println("Usuario com - " + id + " - Excluido com Sucesso");
+
+	}
+
+	public static void testBuscarPorId(Scanner teclado, UsuarioDAO usuDao) {
+
+		System.out.println("Digite o Id do Usuario a ser buscado");
+		Integer id = teclado.nextInt();
+
+		Usuario usuRetornado = usuDao.buscarPorId(id);
+
+		if (usuRetornado != null) {
+
+			System.out.println("Nome:  " + usuRetornado.getNome());
+			System.out.println("Login: " + usuRetornado.getLogin());
+			System.out.println("Senha: " + usuRetornado.getSenha());
+			System.out.println("Id: " + usuRetornado.getId());
+
+		}
+	}
+	public static void testBuscarTodos(UsuarioDAO usuDao) {
+
+		List<Usuario> lista= usuDao.buscarTodos();
+
+		for(Usuario usu: lista) {
+			System.out.println(" ");
+			System.out.println("Id: " + usu.getId());
+			System.out.println("Nome:  " + usu.getNome());
+			System.out.println("Login: " + usu.getLogin());
+			System.out.println("Senha: " + usu.getSenha());
+			System.out.println(" ");
+		}
+	}
+	public static void testAutenticar(Scanner teclado, UsuarioDAO usuDao, Usuario usu) {
+
+		System.out.println("Digite o Login do Usuario: ");
+		usu.setLogin(teclado.nextLine());
+
+		System.out.println("Digite a senha do Usuario: ");
+		usu.setSenha(teclado.nextLine());
+
+		Usuario usuRetornado = usuDao.autenticar(usu);
+
+		if (usuRetornado != null) {
+
+			System.out.println("Login: " + usuRetornado.getLogin());
+			System.out.println("Senha: " + usuRetornado.getSenha());
+			
+		}
+	}
+
 }
